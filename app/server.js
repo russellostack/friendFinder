f240dafffbddfc5ad310bb79cd9724398584e5c8
russellostack <32462495+russellostack@users.noreply.github.com>
2019-04-25 13:55:43 -0400

    package and server.js added

---
+++ app/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "app/html/")));
+
+require("./app/routing/apirouts")(app);
+require("./app/routing/htmlrouts")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT "+PORT);
+});
