d661efa39af6612bafdc8496aed7d096e603ed61
russellostack <32462495+russellostack@users.noreply.github.com>
2019-04-25 19:31:31 -0400

    fixed bad structure set up and express.static issues

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "app/html/")));
+app.use(express.static(path.join(__dirname, "app/public/")));
 
-require("./routing/apirouts")(app);
-require("./routing/htmlrouts")(app);
+require("./app/routing/apirouts")(app);
+require("./app/routing/htmlrouts")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT "+PORT);
