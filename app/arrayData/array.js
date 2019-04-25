2c544cdbe0c3f08e9f1f8644bb411a0d6310ea56
russellostack <32462495+russellostack@users.noreply.github.com>
2019-04-25 15:39:50 -0400

    friend array added and populated with GOT chars, onclick button added to html

---
+++ app/arrayData/array.js
@@ -0,0 +1,63 @@
+var friendsArray = [
+    {
+        name:"Tyrion Lannister",
+        photo:"https://www.imdb.com/name/nm0227759/mediaviewer/rm307862016",
+        scores:[4,2,1,4,5,3,2,5,1,3]
+    },
+    {
+        name:"Cersi Lannister",
+        photo:"https://www.imdb.com/name/nm0372176/mediaviewer/rm126344960",
+        scores:[3,2,1,2,3,4,4,3,2,1]
+    },
+    {
+        name:"Daenerys Targaryen",
+        photo:"https://www.imdb.com/name/nm3592338/mediaviewer/rm699918848",
+        scores:[1,2,2,1,2,3,4,5,4,5]
+    },
+    {
+        name:"Jon Snow",
+        photo:"https://www.imdb.com/name/nm3229685/mediaviewer/rm94420224",
+        scores:[4,5,4,5,4,3,4,5,3,2]
+    },
+    {
+        name:"Sansa Stark",
+        photo:"https://www.imdb.com/name/nm3849842/mediaviewer/rm4217291520",
+        scores:[5,1,3,2,4,2,1,2,2,1]
+    },
+    {
+        name:"Arya Stark",
+        photo:"https://www.imdb.com/name/nm3586035/mediaviewer/rm3469058304",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name:"Jamie Lannister",
+        photo:"https://www.imdb.com/name/nm0182666/mediaviewer/rm1772655104",
+        scores:[3,2,4,2,1,2,2,1,3,5]
+    },
+    {
+        name:"Jorah Mormont",
+        photo:"https://www.imdb.com/name/nm0322513/mediaviewer/rm1379434496",
+        scores:[4,3,2,1,1,2,4,2,1,5,]
+    },
+    {
+        name:"Theon Greyjoy",
+        photo:"https://www.imdb.com/name/nm0654295/mediaviewer/rm2709173760",
+        scores:[5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name:"Samwell Tarly",
+        photo:"https://www.imdb.com/name/nm4263213/mediaviewer/rm1697619968",
+        scores:[5,1,2,1,3,5,2,3,4,5]
+    },
+    {
+        name:"Bran Stark",
+        photo:"https://www.imdb.com/name/nm3652842/mediaviewer/rm1907159296",
+        scores:[5,4,5,4,3,4,5,3,4,5]
+    },
+    {
+        name:"Lady Brienne",
+        photo:"https://www.imdb.com/name/nm3729225/mediaviewer/rm2994386432",
+        scores:[3,4,5,3,2,3,4,5,2,1]
+    },
+    
+]
